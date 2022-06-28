24bf5699a12fd938a75e56429f679980f5fb3899
yangrd <2445213759@qq.com>
2022-06-28 11:15:42 +0800

    feature: support channel

--- src/procedure/concurrency/mod.rs
+++ src/procedure/concurrency/mod.rs
@@ -46,7 +46,7 @@ fn make_barrier(apply_args: &mut ApplyArgs) -> LispType {
     }
 }
 
-fn barrier_await(apply_args: &mut ApplyArgs) -> LispType {
+fn barrier_wait(apply_args: &mut ApplyArgs) -> LispType {
     apply_args.check_args_num(1);
     let car = apply_args.args().car();
     if let Concurrency(ConcurrencyBox::BARRIER(barrier)) = car {
@@ -74,10 +74,23 @@ fn make_channel(apply_args: &mut ApplyArgs) -> LispType {
 
 fn channel_send(apply_args: &mut ApplyArgs) -> LispType {
     apply_args.check_args_num(2);
-    let car = apply_args.args().car();
-    let cdr = apply_args.args().cdr();
+    let list = apply_args.args();
+    let car = list.car();
+    let cdr = list.cdr();
     if let Concurrency(ConcurrencyBox::Channel(tx, rx)) = car {
         tx.send(cdr.car()).expect("channel send error");
+        Nil
+    } else {
+        panic!("channel-send: not a channel");
+    }
+}
+
+fn channel_done(apply_args: &mut ApplyArgs) -> LispType {
+    apply_args.check_args_num(1);
+    let list = apply_args.args();
+    let car = list.car();
+    if let Concurrency(ConcurrencyBox::Channel(tx, rx)) = car {
+        tx.send(Symbol("done".to_string())).expect("channel send error");
         Nil
     } else {
         panic!("channel-send: not a channel");
@@ -94,16 +107,22 @@ fn channel_recv(apply_args: &mut ApplyArgs) -> LispType {
     }
 }
 
+
 fn channel_foreach(apply_args: &mut ApplyArgs) -> LispType {
     apply_args.check_args_num(2);
-    let car = apply_args.args().car();
-    let cdr = apply_args.args().cdr().car();
-    if let Concurrency(ConcurrencyBox::Channel(tx, rx)) = car {
-        if let Procedure(proc)=cdr{
-            rx.iter().for_each(|x| {
+    let list = apply_args.args();
+    let proc =list.car();
+    let chan = list.cdr().car();
+    if let Concurrency(ConcurrencyBox::Channel(tx, rx)) = chan {
+        drop(tx);
+        if let Procedure(proc)=proc{
+            for x in rx.iter() {
+                if(x==Symbol("done".to_string())){
+                    return Nil;
+                }
                 let mut apply_args0 = apply_args.clone_of(Some(List::of(vec![x])));
                 proc.read().expect("proc error")(&mut apply_args0);
-            });
+            }
             Nil
         }else {
             panic!("channel-for-each: not a procedure");
@@ -113,15 +132,21 @@ fn channel_foreach(apply_args: &mut ApplyArgs) -> LispType {
     }
 }
 
+
 pub fn reg_procedure(env: &mut Env) {
     env.reg_procedure("thread-run", thread_run);
     env.reg_procedure("sleep", sleep);
     // env.reg_procedure("thread-join", thread_join);
     env.reg_procedure("make-lock", make_lock);
     env.reg_procedure("make-barrier", make_barrier);
-    env.reg_procedure("barrier-await", barrier_await);
+    env.reg_procedure("barrier-wait", barrier_wait);
     env.reg_procedure("make-channel", make_channel);
     env.reg_procedure("channel-send", channel_send);
+    env.reg_procedure("->", channel_send);
+    env.reg_procedure("channel-done", channel_done);
     env.reg_procedure("channel-recv", channel_recv);
+    env.reg_procedure("<-", channel_recv);
     env.reg_procedure("channel-for-each", channel_foreach);
+    env.reg_procedure("<--", channel_foreach);
+
 }
