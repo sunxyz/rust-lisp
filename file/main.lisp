0574bd911873b05bdeb96fd8a36c6e48e751f144
yangrd <2445213759@qq.com>
2022-07-02 11:50:00 +0800

    call-with-tcp-listener

--- file/main.lisp
+++ file/main.lisp
@@ -4,7 +4,15 @@
     ;; (println  tcp-listener)
     ;; (tcp-listener 8088)
     (def req-read-string  (in) (byte-vector->string (read-byte-vector in 128)))
-    (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in) (
+    (def handler (in out) (
+        ;; (sleep 1)
         (display  (req-read-string in))
-        "HTTP/1.1 200 OK\r\n\r\n hello word")))
+        (write-string "HTTP/1.1 200 OK\r\n\r\n hello word" out )
+    ))
+    ;; (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in out) (
+    ;;      (thread-run (lambda () (handler in out)))
+    ;; )))
+    (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in out) (
+         ( handler in out)
+    )))
 )
