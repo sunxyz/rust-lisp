c35ae52db17722ca76bcccf1a431d5d6fe2c88a2
yangrd <2445213759@qq.com>
2022-07-02 11:52:21 +0800

    -

--- file/main.lisp
+++ file/main.lisp
@@ -9,10 +9,17 @@
         (display  (req-read-string in))
         (write-string "HTTP/1.1 200 OK\r\n\r\n hello word" out )
     ))
+    ;; sample 1
     ;; (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in out) (
     ;;      (thread-run (lambda () (handler in out)))
     ;; )))
-    (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in out) (
-         ( handler in out)
+    ;; sample 2
+    ;; (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in out) (
+    ;;      ( handler in out)
+    ;; )))
+    ;; sample 3
+    (call-with-tcp-listener "127.0.0.1:8088" ( lambda (in) (
+         (display  (req-read-string in))
+         "HTTP/1.1 200 OK\r\n\r\n hello word"
     )))
 )
